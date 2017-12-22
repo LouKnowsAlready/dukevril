c3a0b127800744265609e420e4559074ca22e47a
lourence.l.cabaluna <lourence.l.cabaluna@gmail.com>
2017-12-22 10:29:52 +0800

    Adding carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206071757) do
+ActiveRecord::Schema.define(version: 20171218093747) do
 
   create_table "academies", force: :cascade do |t|
     t.string "academy_name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20171206071757) do
     t.string "country"
     t.integer "academy_id"
     t.boolean "isAdmin", default: false
+    t.string "avatar"
     t.index ["academy_id"], name: "index_users_on_academy_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
